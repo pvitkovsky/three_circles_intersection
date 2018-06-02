b2a92192ad7310b0610c458f262f599b0dbc5fe3
Denis Galimov <dis@Slony>
2018-06-02 18:10:42 +0300

    IMHO: No needed to create instance of TestRunner

--- src/com/test/jtest_circles/Main.java
+++ src/com/test/jtest_circles/Main.java
@@ -3,7 +3,7 @@ package com.test.jtest_circles;
 
 public class Main {
   public static void main(String[] args) {
-    TestRunner tr = new TestRunner();
-    tr.main();
+    //TestRunner tr = new TestRunner();
+    TestRunner.main();
   }
 }
